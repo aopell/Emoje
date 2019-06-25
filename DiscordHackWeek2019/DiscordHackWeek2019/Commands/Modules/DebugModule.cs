08cd70042ca6ab7d6158321ade6397479a2b4121
Aaron Opell <aaron.opell@live.com>
2019-06-25 00:57:36 -0700

    Debugmodule

--- DiscordHackWeek2019/DiscordHackWeek2019/Commands/Modules/DebugModule.cs
+++ DiscordHackWeek2019/DiscordHackWeek2019/Commands/Modules/DebugModule.cs
@@ -6,25 +6,13 @@ using System.Threading.Tasks;
 
 namespace DiscordHackWeek2019.Commands.Modules
 {
-    [Group("trade")]
+    [Group("debug")]
     public class DebugModule : ModuleBase<BotCommandContext>
     {
-        [Command("offer"), Alias("create"), Summary("Create a trade offer to send to another user")]
+        [Command("emojilist")]
         public Task Offer()
         {
-            throw new NotImplementedException();
-        }
 
-        [Command("accept"), Summary("Accept a trade offer from another player")]
-        public Task Accept()
-        {
-            throw new NotImplementedException();
-        }
-
-        [Command("decline"), Summary("Decline a trade offer from another player")]
-        public Task Decline()
-        {
-            throw new NotImplementedException();
         }
     }
 }
