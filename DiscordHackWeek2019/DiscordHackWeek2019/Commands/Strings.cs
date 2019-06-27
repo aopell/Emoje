dbaf2c2bdeb6aed2c7b69c224bbb6959a52a74ad
atomicproton <imim123345667899@hotmail.com>
2019-06-26 23:50:07 -0700

    Rarity strings

--- DiscordHackWeek2019/DiscordHackWeek2019/Commands/Strings.cs
+++ DiscordHackWeek2019/DiscordHackWeek2019/Commands/Strings.cs
@@ -16,5 +16,15 @@ namespace DiscordHackWeek2019.Commands
         public const string transactionEmoji = "<:transaction:593608952768626709>";
         public const string stockEmoji = "<:stocks:593606084460281867>";
         public const string cryptoEmoji = "<:crypto:593603698513412098>";
+
+        // Rarities
+        public const string commonLeft = "<:common_l:593689834778918937>";
+        public const string commonRight = "<:common_r:593690111435341827>";
+        public const string rareLeft = "<:rare_l:593689863275020288>";
+        public const string rareRight = "<:rare_r:593690111951241236>";
+        public const string epicLeft = "<:epic_l:593689846921429026>";
+        public const string epicRight = "<:epic_r:593690111628279809>";
+        public const string legendaryLeft = "<:legendary_l:593691613969121280>";
+        public const string legendaryRight = "<:legendary_r:593691673289162774>";
     }
 }
