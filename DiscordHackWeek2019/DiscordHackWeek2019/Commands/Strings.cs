10737885e5b8396557130ab8ccbdb66f7d745cc8
atomicproton <imim123345667899@hotmail.com>
2019-06-26 18:25:43 -0700

    Profile emoji maybe

--- DiscordHackWeek2019/DiscordHackWeek2019/Commands/Strings.cs
+++ DiscordHackWeek2019/DiscordHackWeek2019/Commands/Strings.cs
@@ -6,6 +6,15 @@ namespace DiscordHackWeek2019.Commands
 {
     public static class Strings
     {
+        // Messages
         public const string UserJoinNeeded = "The given user has not joined";
+
+        // Emoji
+        public const string moneyEmoji = "<:money:593606876051013678>";
+        public const string emojiEmoji = "😃";
+        public const string boxEmoji = "<:lootbox:593607880251277322>";
+        public const string transactionEmoji = "<:transaction:593608952768626709>";
+        public const string stockEmoji = "<:stocks:593606084460281867>";
+        public const string cryptoEmoji = "<:crypto:593603698513412098>";
     }
 }
