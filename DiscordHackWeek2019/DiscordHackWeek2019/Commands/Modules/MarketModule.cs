a8b1b4bec0252b0ed46bfd897658c1429f191002
JozsefA <jozsefaug@gmail.com>
2019-06-28 00:10:08 -0700

    Quick fix

--- DiscordHackWeek2019/DiscordHackWeek2019/Commands/Modules/MarketModule.cs
+++ DiscordHackWeek2019/DiscordHackWeek2019/Commands/Modules/MarketModule.cs
@@ -28,9 +28,26 @@ namespace DiscordHackWeek2019.Commands.Modules
         public Task ViewListings() => ViewListings(Market.Global);
 
         [Command("listings"), Alias("view"), Summary("Views market listings in either the global or local markets")]
-        public Task ViewListings(Market market)
+        public async Task ViewListings(Market market, string emoji = "all", [Remainder] string sorting = "lowest")
         {
-            throw new NotImplementedException();
+            switch (sorting)
+            {
+                case "highest":
+                case "highest to lowest":
+                case "greatest to least":
+                case "g2l":
+                case "h2l":
+                    break;
+                case "lowest":
+                case "lowest to highest":
+                case "lowest to ":
+                case "cheap":
+                case "l2g":
+                case "l2h":
+                default:
+
+                    break;
+            }
         }
 
         [Command("buy"), Alias("purchase", "order"), Summary("Purchase an emoji from the global market")]
@@ -91,7 +108,7 @@ namespace DiscordHackWeek2019.Commands.Modules
             }
 
             var message = await ReplyAsync($"{Context.User.Mention}, are you sure you want to sell {emoji} for {Context.Money(price)}?");
-            ReactionMessageHelper.CreateReactionMessage(Context, message,
+            ReactionMessageHelper.CreateConfirmReactionMessage(Context, message,
                 async r =>
                 {
                     var (toSell, index) = inventory.Enumerate(emoji).Select((e, i) => (e, i)).OrderBy(e => e.e.Transactions.Count).First();
@@ -100,8 +117,12 @@ namespace DiscordHackWeek2019.Commands.Modules
 
                     MarketHelper.AddListing(Context, MarketId(market), toSell, price);
                     inventory.Save();
-                    await ReplyAsync($"Posted your {emoji} for {Context.Money(price)} in {Context.GetMarketName(MarketId(market))}");
-                }, r => Task.CompletedTask);
+                    await message.ModifyAsync(m => m.Content = $"Listed your {emoji} for {Context.Money(price)} in {Context.GetMarketName(MarketId(market))}");
+                }, 
+                async r =>
+                {
+                    await message.ModifyAsync(m => m.Content = $"Cancelled listing of {emoji}");
+                });
         }
     }
 }
