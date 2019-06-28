1811eefe6ccd23b13c223fb8098e4c74fc1be564
atomicproton <imim123345667899@hotmail.com>
2019-06-28 01:05:17 -0700

    Updated emoji

--- DiscordHackWeek2019/DiscordHackWeek2019/Commands/Strings.cs
+++ DiscordHackWeek2019/DiscordHackWeek2019/Commands/Strings.cs
@@ -19,12 +19,12 @@ namespace DiscordHackWeek2019.Commands
         public const string cryptoEmoji = "<:crypto:593603698513412098>";
 
         // Rarities
-        public const string commonLeft = "<:common_l:593993338261078026>";
-        public const string commonRight = "<:common_r:593993401414975499>";
-        public const string rareLeft = "<:rare_l:593689863275020288>";
-        public const string rareRight = "<:rare_r:593690111951241236>";
-        public const string epicLeft = "<:epic_l:593689846921429026>";
-        public const string epicRight = "<:epic_r:593690111628279809>";
+        public const string commonLeft = "<:blue_l:593992724504641541>";
+        public const string commonRight = "<:blue_r:593992909985021952>";
+        public const string rareLeft = "<:green_l:593689834778918937>";
+        public const string rareRight = "<:green_r:593690111435341827>";
+        public const string epicLeft = "<:image7:593993152076054529>";
+        public const string epicRight = "<:image7:593993222276251648>";
         public const string legendaryLeft = "<:legendary_l:593691613969121280>";
         public const string legendaryRight = "<:legendary_r:593691673289162774>";
     }
